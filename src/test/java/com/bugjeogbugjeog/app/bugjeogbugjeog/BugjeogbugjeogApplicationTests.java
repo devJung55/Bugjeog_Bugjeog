2b1f6752a246e9fb35d4f34e549add72c6c2d125
code-leesooncheol <2sooncheol@gmail.com>
2023-03-10 17:45:56 +0900

    spring project start

---
+++ src/test/java/com/bugjeogbugjeog/app/bugjeogbugjeog/BugjeogbugjeogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bugjeogbugjeog.app.bugjeogbugjeog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BugjeogbugjeogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
