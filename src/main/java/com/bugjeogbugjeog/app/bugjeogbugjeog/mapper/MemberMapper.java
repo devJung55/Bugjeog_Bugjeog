451cdb6fe67bdc94006037a3f4ed8dc9383808d3
devJung55 <devJung55@gmail.com>
2023-03-23 17:20:16 +0900

    정지영 - 백엔드 설계 수정

--- src/main/java/com/bugjeogbugjeog/app/bugjeogbugjeog/mapper/MemberMapper.java
+++ src/main/java/com/bugjeogbugjeog/app/bugjeogbugjeog/mapper/MemberMapper.java
@@ -1,8 +1,11 @@
 package com.bugjeogbugjeog.app.bugjeogbugjeog.mapper;
 
+import com.bugjeogbugjeog.app.bugjeogbugjeog.domain.vo.MemberVO;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 
 @Mapper
 public interface MemberMapper {
+//    자영업자 회원가입
+    public void insertMember(MemberVO memberVO);
 }
