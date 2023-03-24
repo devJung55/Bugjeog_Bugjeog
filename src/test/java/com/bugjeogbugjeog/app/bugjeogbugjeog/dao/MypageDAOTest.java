80023f0963e4bd4542784266868ee0f560d5d189
WookCoding <dnrwhddla@gmail.com>
2023-03-24 10:53:19 +0900

    ref : myPage dao

--- src/test/java/com/bugjeogbugjeog/app/bugjeogbugjeog/dao/MypageDAOTest.java
+++ src/test/java/com/bugjeogbugjeog/app/bugjeogbugjeog/dao/MypageDAOTest.java
@@ -2,6 +2,7 @@ package com.bugjeogbugjeog.app.bugjeogbugjeog.dao;
 
 
 import com.bugjeogbugjeog.app.bugjeogbugjeog.domain.dao.MypageDAO;
+import com.bugjeogbugjeog.app.bugjeogbugjeog.domain.vo.MemberVO;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,5 +18,17 @@ public class MypageDAOTest {
     @Test
     public void findByIdTest(){
         log.info(mypageDAO.findById(1L).toString());
+    }
+
+    @Test
+    public void updateByIdTest(){
+        MemberVO memberVO = mypageDAO.findById(1L);
+
+        memberVO.setMemberEmail("hs@naver.com");
+        memberVO.setMemberImgOriginalName("12345");
+        memberVO.setMemberImgPath("12345");
+        memberVO.setMemberImgUuid("12345");
+
+        mypageDAO.updateById(memberVO);
     }
 }
