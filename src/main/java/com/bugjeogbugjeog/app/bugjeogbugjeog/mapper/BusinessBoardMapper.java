327fd08d4a492f1e356c50ce8bc5a6051cd6ca63
LeeJunSang <miraclelee0613@gmail.com>
2023-03-24 11:36:46 +0900

    boardBusiness작업중

--- src/main/java/com/bugjeogbugjeog/app/bugjeogbugjeog/mapper/BusinessBoardMapper.java
+++ src/main/java/com/bugjeogbugjeog/app/bugjeogbugjeog/mapper/BusinessBoardMapper.java
@@ -1,7 +1,18 @@
 package com.bugjeogbugjeog.app.bugjeogbugjeog.mapper;
 
+import com.bugjeogbugjeog.app.bugjeogbugjeog.domain.dto.BoardBusinessDTO;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
 
 @Mapper
 public interface BusinessBoardMapper {
+    //    추가
+//    public void insert(OrderVO orderVO);
+    //    삭제
+//    public void delete(Long orderId);
+    //    조회(총 결제 금액까지)
+//    public OrderDTO select(Long orderId);
+    //    목록(총 결제 금액까지)
+    public List<BoardBusinessDTO> selectAll();
 }
--- src/main/java/com/bugjeogbugjeog/app/bugjeogbugjeog/service/BusinessBoardService.java
+++ src/main/java/com/bugjeogbugjeog/app/bugjeogbugjeog/service/BusinessBoardService.java
@@ -1,7 +1,18 @@
 package com.bugjeogbugjeog.app.bugjeogbugjeog.service;
 
+import com.bugjeogbugjeog.app.bugjeogbugjeog.domain.dao.BusinessBoardDAO;
+import com.bugjeogbugjeog.app.bugjeogbugjeog.domain.dao.BusinessBoardImgDAO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
 @Service
+@Qualifier("businessBoard")
+@Primary
+@RequiredArgsConstructor
 public class BusinessBoardService {
+    private final BusinessBoardDAO businessBoardDAO;
+    private final BusinessBoardImgDAO businessBoardImgDAO;
+
 }
