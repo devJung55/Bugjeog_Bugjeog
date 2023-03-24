8dc96f914b4bfaffa80ee5045ab37ef7b8c0abaf
WookCoding <dnrwhddla@gmail.com>
2023-03-24 11:13:49 +0900

    ref : myPageService

--- src/test/java/com/bugjeogbugjeog/app/bugjeogbugjeog/service/MyPageServiceTests.java
+++ src/test/java/com/bugjeogbugjeog/app/bugjeogbugjeog/service/MyPageServiceTests.java
@@ -1,6 +1,7 @@
 package com.bugjeogbugjeog.app.bugjeogbugjeog.service;
 
 
+import com.bugjeogbugjeog.app.bugjeogbugjeog.domain.vo.MemberVO;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,9 +14,19 @@ public class MyPageServiceTests {
     @Autowired
     MyPageService myPageService;
 
+//   회원정보 조회
     @Test
     public void memberInfoTest(){
         log.info(myPageService.memberInfo(1L).toString());
     }
 
+    @Test
+    public void memberUpdateTest(){
+        MemberVO memberVO = myPageService.memberInfo(1L);
+
+        memberVO.setMemberPhoneNumber("010-4444-5555");
+        myPageService.memberUpdate(memberVO);
+    }
+
+    
 }
