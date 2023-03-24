fab42986223f7123af1da65132f8595ea46b54dc
dev-Sungyu <tjfdhs364@gmail.com>
2023-03-24 16:15:20 +0900

    커밋

--- src/main/java/com/bugjeogbugjeog/app/bugjeogbugjeog/controller/TestController.java
+++ src/main/java/com/bugjeogbugjeog/app/bugjeogbugjeog/controller/TestController.java
@@ -6,18 +6,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/mypage/*")
+@RequestMapping("/board/*")
 /*@RequestMapping("/main/*")*/
 public class TestController {
 
-    @GetMapping("/profile/favorite")
-    public String main(){
-        return "mypage/specific/personalFavoriteList";
-    }
-    @GetMapping("/profile/edit")
-    public String main2(){
-        return "mypage/specific/businessEdit";
-    }
+    @GetMapping("/pr-")
+
+//    @GetMapping("/profile/favorite")
+//    public String main(){
+//        return "mypage/specific/personalFavoriteList";
+//    }
+//    @GetMapping("/profile/edit")
+//    public String main2(){
+//        return "mypage/specific/businessEdit";
+//    }
 
 /*    @GetMapping("/main")
     public void main(){
