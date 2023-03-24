aa6392b954cff05cff5d62cbad750cad4c4a1a5e
WookCoding <dnrwhddla@gmail.com>
2023-03-24 09:11:00 +0900

    ref : myPageService

--- src/test/java/com/bugjeogbugjeog/app/bugjeogbugjeog/service/MyPageServiceTests.java
+++ src/test/java/com/bugjeogbugjeog/app/bugjeogbugjeog/service/MyPageServiceTests.java
@@ -2,9 +2,20 @@ package com.bugjeogbugjeog.app.bugjeogbugjeog.service;
 
 
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 @Slf4j
-public class MyPageService {
+public class MyPageServiceTests {
+
+    @Autowired
+    MyPageService myPageService;
+
+    @Test
+    public void memberInfoTest(){
+        log.info(myPageService.memberInfo(1L).toString());
+    }
+
 }
