84375f1b8b75728bfab4afc3c402f4dbdd3d25c0
dev-Sungyu <tjfdhs364@gmail.com>
2023-03-26 23:23:39 +0900

    보드 헤더 수정

--- src/main/java/com/bugjeogbugjeog/app/bugjeogbugjeog/controller/MyPageSpecificController.java
+++ src/main/java/com/bugjeogbugjeog/app/bugjeogbugjeog/controller/MyPageSpecificController.java
@@ -1,26 +1,36 @@
 package com.bugjeogbugjeog.app.bugjeogbugjeog.controller;
 
 
+import com.bugjeogbugjeog.app.bugjeogbugjeog.service.MyPageService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/mypage/*")
-/*@RequestMapping("/main/*")*/
+@RequestMapping("/board/*")
+@RequiredArgsConstructor
 public class MyPageSpecificController {
 
-
-    @GetMapping("/profile/favorite")
+    @GetMapping("/test")
     public String main(){
-        return "mypage/specific/personalFavoriteList";
+        return "board/business/pr-boardList-test";
     }
-    @GetMapping("/profile/edit")
+    @GetMapping("/test2")
     public String main2(){
-        return "mypage/specific/businessEdit";
+        return "board/business/write";
     }
 
-/*    @GetMapping("/main")
-    public void main(){
-    }*/
+//    지역선택과 카테고리 선택 후 적용하기 눌렀을때 여기로 날아옴
+//    @PostMapping("/specific/personalFavoriteList")
+//    public String locaCate(){
+//        return
+//    }
+
+//    @GetMapping("/specific/businessEdit")
+//    public String main2(){
+//        return "mypage/specific/businessEdit";
+//    }
+
 }
