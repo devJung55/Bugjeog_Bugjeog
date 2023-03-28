acbbc48ec836d6f8e68a11a6446c939dc3a2bf41
dev-Sungyu <tjfdhs364@gmail.com>
2023-03-28 18:34:47 +0900

    매퍼 테스트

--- src/test/java/com/bugjeogbugjeog/app/bugjeogbugjeog/mapper/InterestingCompanyMapperTest.java
+++ src/test/java/com/bugjeogbugjeog/app/bugjeogbugjeog/mapper/InterestingCompanyMapperTest.java
@@ -2,9 +2,19 @@ package com.bugjeogbugjeog.app.bugjeogbugjeog.mapper;
 
 
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 @Slf4j
 public class InterestingCompanyMapperTest {
+
+    @Autowired
+    InterestingCompanyMapper interestingCompanyMapper;
+
+    @Test
+    public void selectInterestingCompanyTest(){
+        log.info(interestingCompanyMapper.selectInterestingCompany().toString());
+    }
 }
