99313687e85c09ec3f87dc31db9fa23bdb1290a6
dev-Sungyu <tjfdhs364@gmail.com>
2023-03-28 17:47:36 +0900

    interestingCompany mapper 완료

--- src/main/java/com/bugjeogbugjeog/app/bugjeogbugjeog/mapper/InterestingCompanyMapper.java
+++ src/main/java/com/bugjeogbugjeog/app/bugjeogbugjeog/mapper/InterestingCompanyMapper.java
@@ -1,7 +1,13 @@
 package com.bugjeogbugjeog.app.bugjeogbugjeog.mapper;
 
+import com.bugjeogbugjeog.app.bugjeogbugjeog.domain.dto.InterestingCompanyDTO;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
 
 @Mapper
 public interface InterestingCompanyMapper {
+    //         관심업체 리스트
+    public List<InterestingCompanyDTO> selectInterestingCompany();
+
 }
