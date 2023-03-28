52b5ccb0f3181f7f8987a37be37785c4aca4e5b7
WookCoding <dnrwhddla@gmail.com>
2023-03-28 21:23:46 +0900

    중간 커밋

--- src/main/resources/static/js/mypage/modal.js
+++ src/main/resources/static/js/mypage/modal.js
@@ -29,6 +29,15 @@ $memberPhoneModalButton.click(() => {
 
 $memberPhoneModalExit.each((i, e) => {
     $(e).click(() => {
+        $(".phoneNumber").text(phoneNumber(memberVO.memberPhoneNumber));
+        $("input[name=memberPhoneNumber]").val(memberVO.memberPhoneNumber);
+        $("input[name=memberPhoneNumber]").attr("readonly", true);
+        $("input[name=memberPhoneNumber]").attr("disabled", true);
+        $(".code-check-input").attr("readonly", true);
+        $(".code-check-input").val("");
+        $errorMsg.text("");
+        $(".change-phoneNumber").show();
+        $codeButton.hide();
         $memberPhoneModal.hide();
     });
 })
@@ -45,4 +54,22 @@ $fileExitButton.click(function(){
 $fileCancelButton.click(function(e){
     e.preventDefault();
     $fileModal.hide();
+});
+
+// 비밀번호 변경 모달
+const $passwordModalButton = $(".password-change-modal");
+const $passwordModal = $(".password-modal-display");
+const $passwordModalExit = $(".password-modal-exit");
+const $passwordCancel = $(".password-cancel");
+
+$passwordModalButton.click(function(){
+    $passwordModal.show();
+});
+
+$passwordModalExit.each((i, e) => {
+    $(e).click(function() {
+        $("input[type=password]").val("");
+        $errorMsg.text("");
+        $passwordModal.hide();
+    });
 });
