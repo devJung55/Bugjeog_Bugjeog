1ff207222e9e439ed07191bcee7c87198d06514b
WookCoding <dnrwhddla@gmail.com>
2023-03-28 11:54:35 +0900

    ref: BusinessMapper, BusinessDAO

--- src/main/java/com/bugjeogbugjeog/app/bugjeogbugjeog/domain/dao/ReplyDAO.java
+++ src/main/java/com/bugjeogbugjeog/app/bugjeogbugjeog/domain/dao/ReplyDAO.java
@@ -1,7 +1,27 @@
 package com.bugjeogbugjeog.app.bugjeogbugjeog.domain.dao;
 
+import com.bugjeogbugjeog.app.bugjeogbugjeog.domain.dto.MyPageReplyDTO;
+import com.bugjeogbugjeog.app.bugjeogbugjeog.domain.vo.Criteria;
+import com.bugjeogbugjeog.app.bugjeogbugjeog.mapper.ReplyMapper;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
+@RequiredArgsConstructor
 public class ReplyDAO {
+
+    private final ReplyMapper replyMapper;
+
+    //  댓글 단 게시물 목록
+    public List<MyPageReplyDTO> findAllMyPageReplyDTO(Long memberId, Criteria criteria){
+        return replyMapper.replyList(memberId, criteria);
+    };
+
+    // 댓글 갯수
+    public Integer getReplyTotal(Long memberId){
+        return replyMapper.replyCount(memberId);
+    };
+
 }
