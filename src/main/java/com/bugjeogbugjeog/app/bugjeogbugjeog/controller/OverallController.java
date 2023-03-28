b1b9567ab00c809bfd1d91528521714c83ef4dcd
jeffy1217 <younjune1217@gmail.com>
2023-03-29 08:48:05 +0900

    헤더 링크 연결 진행 중

---
+++ src/main/java/com/bugjeogbugjeog/app/bugjeogbugjeog/controller/OverallController.java
@@ -0,0 +1,31 @@
+package com.bugjeogbugjeog.app.bugjeogbugjeog.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/overall/*")
+@Slf4j
+public class OverallController {
+
+    //    헤더쪽 유통업체 business의 list
+    @GetMapping("distribution")
+    public String moveDistribution(){ return "/board/business/list";}
+
+    //    헤더쪽 문의하기 inquiry의 boardList
+    @GetMapping("inquiry")
+    public String moveInquiry(){ return "/board/inquiry/boardList";}
+
+    //     헤더쪽 공지사항 notice의 list
+    @GetMapping("notice")
+    public String moveNotice(){ return "/board/notice/list";}
+
+    //      헤더쪽 FAQ
+    @GetMapping("faq")
+    public String moveFaq(){ return "/notice/faq";}
+
+}
