59e00fe44cea6fb4c0cd922bd1c68dd705b0dfae
dev-Sungyu <tjfdhs364@gmail.com>
2023-03-28 19:11:56 +0900

    중간 커밋

--- src/test/java/com/bugjeogbugjeog/app/bugjeogbugjeog/mapper/InterestingCompanyMapperTest.java
+++ src/test/java/com/bugjeogbugjeog/app/bugjeogbugjeog/mapper/InterestingCompanyMapperTest.java
@@ -15,6 +15,5 @@ public class InterestingCompanyMapperTest {
 
     @Test
     public void selectInterestingCompanyTest(){
-        log.info(interestingCompanyMapper.selectInterestingCompany().toString());
     }
 }
