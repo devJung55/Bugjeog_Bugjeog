0d115ccffd18df14b26eab5b4f8caaef47ba99fb
LeeJunSang <miraclelee0613@gmail.com>
2023-03-29 20:01:05 +0900

    작업중

---
+++ src/main/java/com/bugjeogbugjeog/app/bugjeogbugjeog/service/BoardBusinessImgService.java
@@ -0,0 +1,26 @@
+package com.bugjeogbugjeog.app.bugjeogbugjeog.service;
+
+import com.bugjeogbugjeog.app.bugjeogbugjeog.domain.dao.BusinessBoardImgDAO;
+import com.bugjeogbugjeog.app.bugjeogbugjeog.domain.vo.BoardBusinessImgVO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Qualifier("businessBoardImg")
+@Primary
+@RequiredArgsConstructor
+public class BoardBusinessImgService {
+    private final BusinessBoardImgDAO businessBoardImgDAO;
+
+    public void write(List<BoardBusinessImgVO> boardBusinessImgVOs){
+        boardBusinessImgVOs.forEach(imgVO -> businessBoardImgDAO.save(imgVO));
+    }
+
+    public void registerImg(BoardBusinessImgVO boardBusinessImgVO) {
+        businessBoardImgDAO.save(boardBusinessImgVO);
+    }
+}
