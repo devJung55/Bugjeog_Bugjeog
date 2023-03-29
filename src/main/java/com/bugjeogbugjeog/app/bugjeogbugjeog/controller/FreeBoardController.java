14a1f4f90850fdfbc45ef7d80bbc9f16afb56719
jeffy1217 <younjune1217@gmail.com>
2023-03-29 16:50:47 +0900

    자유게시판 작성(write) 완료

--- src/main/java/com/bugjeogbugjeog/app/bugjeogbugjeog/controller/FreeBoardController.java
+++ src/main/java/com/bugjeogbugjeog/app/bugjeogbugjeog/controller/FreeBoardController.java
@@ -1,21 +1,42 @@
 package com.bugjeogbugjeog.app.bugjeogbugjeog.controller;
 
+import com.bugjeogbugjeog.app.bugjeogbugjeog.domain.vo.BoardFreeVO;
+import com.bugjeogbugjeog.app.bugjeogbugjeog.service.FreeBoardService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
-@RequestMapping("/FreeBoards/")
+@RequestMapping("/FreeBoards/*")
+@RequiredArgsConstructor
 public class FreeBoardController {
+
+    private final FreeBoardService freeBoardService;
+
     /*자유게시판 첫 화면(자유게시물 리스트)*/
     @GetMapping("/")
     public String freeBoard(){return "/board/free/list";} //html 경로
 
     /*자유게시판 디테일 */
-    @GetMapping("/detail")
+    @GetMapping("detail")
     public String freeDetail(){return "/board/free/detail";}
 
     /*자유게시판 작성*/
-    @GetMapping("/write")
-    public String freeWrite(){return "board/free/write";}
+    @GetMapping("write")
+    public String freeWrite(Model model){
+        model.addAttribute(new BoardFreeVO());
+        return "/board/free/write";
+    }
+
+    /*중요!! 참고하기 mapper*/
+    /*작성 후 디비 입력 -> 화면 이동*/
+    @PostMapping("free/insert")
+    public RedirectView freeInsert(BoardFreeVO boardFreeVO){
+        freeBoardService.registerBoard(boardFreeVO);
+        return new RedirectView("/FreeBoards/");
+    }
 }
