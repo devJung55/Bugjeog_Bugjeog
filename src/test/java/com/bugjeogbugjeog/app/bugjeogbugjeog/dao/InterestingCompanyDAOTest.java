f723d583cdad60634d36cc60a1bfd05ee235e6e1
dev-Sungyu <tjfdhs364@gmail.com>
2023-03-29 13:19:16 +0900

    interestingCompanyTest 추가

--- src/test/java/com/bugjeogbugjeog/app/bugjeogbugjeog/dao/InterestingCompanyDAOTest.java
+++ src/test/java/com/bugjeogbugjeog/app/bugjeogbugjeog/dao/InterestingCompanyDAOTest.java
@@ -1,10 +1,20 @@
 package com.bugjeogbugjeog.app.bugjeogbugjeog.dao;
 
 
+import com.bugjeogbugjeog.app.bugjeogbugjeog.domain.dao.InterestingCompanyDAO;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 @Slf4j
 public class InterestingCompanyDAOTest {
+    @Autowired
+    InterestingCompanyDAO interestingCompanyDAO;
+
+    @Test
+    public void InterestingCompanyDAOTest(){
+        log.info(interestingCompanyDAO.findAllToInterestingCompany().toString());
+    }
 }
