d5f117ad741d888a6e60bf0924d93c82e11d9055
LeeSoonCheol <2sooncheol@gmail.com>
2023-03-30 08:49:04 +0900

    비즈니스 DTO 생성

---
+++ src/main/java/com/bugjeogbugjeog/app/bugjeogbugjeog/domain/dto/BusinessDTO.java
@@ -0,0 +1,41 @@
+package com.bugjeogbugjeog.app.bugjeogbugjeog.domain.dto;
+
+import lombok.Data;
+import org.springframework.stereotype.Component;
+
+@Component
+@Data
+public class BusinessDTO {
+    private Long businessId;
+    private String businessEmail;
+    private String businessPassword;
+    private String businessCeoName;
+    private String  businessNumber;
+    private String businessCompanyName;
+    private String businessPhoneNumber;
+    private String businessEstablishmentDate;
+    private String businessRegisterDate;
+    private Long businessStatus;
+    private String businessImgOriginalName;
+    private String businessImgUuid;
+    private String businessImgPath;
+    private String businessLocation;
+    private String businessCategory;
+
+    private Integer boardCount;
+
+    private Long boardFreeId;
+    private Long memberId;
+    private String boardFreeTitle;
+    private String boardFreeContent;
+    private String boardFreeRegisterDate;
+    private String boardFreeUpdateDate;
+    private Long boardFreeLike;
+
+    private Integer replyCount;
+
+    private Long replyId;
+    private String replyContent;
+    private String replyRegisterDate;
+    private String replyUpdateDate;
+}
