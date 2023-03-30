2ebe36f8b4edf2777a7de5f4b648ea4dde051eee
jeffy1217 <younjune1217@gmail.com>
2023-03-30 14:17:09 +0900

    댓글 작업

--- src/main/java/com/bugjeogbugjeog/app/bugjeogbugjeog/controller/FreeBoardController.java
+++ src/main/java/com/bugjeogbugjeog/app/bugjeogbugjeog/controller/FreeBoardController.java
@@ -1,7 +1,9 @@
 package com.bugjeogbugjeog.app.bugjeogbugjeog.controller;
 
 import com.bugjeogbugjeog.app.bugjeogbugjeog.domain.vo.BoardFreeVO;
+import com.bugjeogbugjeog.app.bugjeogbugjeog.domain.vo.FreeReplyVO;
 import com.bugjeogbugjeog.app.bugjeogbugjeog.service.FreeBoardService;
+import com.bugjeogbugjeog.app.bugjeogbugjeog.service.ReplyService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,8 +16,9 @@ import org.springframework.web.servlet.view.RedirectView;
 @RequestMapping("/FreeBoards/*")
 @RequiredArgsConstructor
 public class FreeBoardController {
-
+    /*주입*/
     private final FreeBoardService freeBoardService;
+    private final ReplyService replyService;
 
     /*자유게시판 첫 화면(자유게시물 리스트)*/
     @GetMapping("/")
@@ -38,5 +41,18 @@ public class FreeBoardController {
     public RedirectView freeInsert(BoardFreeVO boardFreeVO){
         freeBoardService.registerBoard(boardFreeVO);
         return new RedirectView("/FreeBoards/");
+    }
+
+    /*댓글 등록*/
+    @GetMapping("resister-reply")
+    public void replyResister(FreeReplyVO freeReplyVO){
+        replyService.save(freeReplyVO);
+    }
+
+    /* 댓글 등록완료*/
+    @GetMapping("reply-complate")
+    public String replyAddEnd(Model model){
+        model.addAttribute(new FreeReplyVO());
+        return "/FreeBoard/resister-reply";
     }
 }
