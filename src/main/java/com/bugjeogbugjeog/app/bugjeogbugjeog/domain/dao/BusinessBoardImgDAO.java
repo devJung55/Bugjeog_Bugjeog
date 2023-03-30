dd5241f9ed9a087a8636cb30c4c44c58f371cbc7
LeeJunSang <miraclelee0613@gmail.com>
2023-03-30 15:12:18 +0900

    유통PR게시판 게시글 작성시 이미지 파일이 1개만 업로드 되는 문제 발견함.

--- src/main/java/com/bugjeogbugjeog/app/bugjeogbugjeog/domain/dao/BusinessBoardImgDAO.java
+++ src/main/java/com/bugjeogbugjeog/app/bugjeogbugjeog/domain/dao/BusinessBoardImgDAO.java
@@ -1,9 +1,13 @@
 package com.bugjeogbugjeog.app.bugjeogbugjeog.domain.dao;
 
+import com.bugjeogbugjeog.app.bugjeogbugjeog.domain.dto.BoardBusinessDTO;
 import com.bugjeogbugjeog.app.bugjeogbugjeog.domain.vo.BoardBusinessImgVO;
 import com.bugjeogbugjeog.app.bugjeogbugjeog.mapper.BusinessBoardImgMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Map;
 
 @Repository
 @RequiredArgsConstructor
@@ -15,6 +19,24 @@ public class BusinessBoardImgDAO {
         businessBoardImgMapper.insert(boardBusinessImgVO);
     }
 
+//    boardBusinessId 이미지 전부
+    public List<BoardBusinessImgVO> findAllByBoardBusinessId(Long boardBusinessId){
+        return businessBoardImgMapper.selectAllByBoardBusinessId(boardBusinessId);
+    }
+
+
+//    public void deleteById(Long boardBusinessId){ businessBoardMapper.delete(boardBusinessId);}
+
+    //    조회(이미지 정보까지)
+//    public BoardBusinessDTO findById(Long boardBusinessId){ return businessBoardMapper.select(boardBusinessId);}
+
+//    public List<BoardBusinessDTO> findByBusinessId(Long businessId){ return businessBoardMapper.selectAllByBusinessId(businessId);}
+
+    //    목록(대표 이미지 하나)
+//    public List<BoardBusinessDTO> findAll(){ return businessBoardMapper.selectAllList(); }
+
+    //    목록(대표 이미지 하나)
+//    public List<BoardBusinessDTO> findAll(Map<String, Object> searchMap){ return businessBoardMapper.selectAllListBySearch(searchMap); }
 
 
 
