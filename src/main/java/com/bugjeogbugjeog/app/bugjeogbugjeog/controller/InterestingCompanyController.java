5d17de4f7aabe2ea3c7158eaadc9310557e5eb0c
dev-Sungyu <tjfdhs364@gmail.com>
2023-03-31 00:52:48 +0900

    delete 추가 중

--- src/main/java/com/bugjeogbugjeog/app/bugjeogbugjeog/controller/InterestingCompanyController.java
+++ src/main/java/com/bugjeogbugjeog/app/bugjeogbugjeog/controller/InterestingCompanyController.java
@@ -4,15 +4,14 @@ package com.bugjeogbugjeog.app.bugjeogbugjeog.controller;
 import com.bugjeogbugjeog.app.bugjeogbugjeog.domain.dto.InterestingCompanyDTO;
 import com.bugjeogbugjeog.app.bugjeogbugjeog.domain.dto.PageDTO;
 import com.bugjeogbugjeog.app.bugjeogbugjeog.domain.vo.Criteria;
+import com.bugjeogbugjeog.app.bugjeogbugjeog.domain.vo.FreeLikeVO;
 import com.bugjeogbugjeog.app.bugjeogbugjeog.service.InterestingCompanyService;
 import com.bugjeogbugjeog.app.bugjeogbugjeog.service.MyPageService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -38,7 +37,7 @@ public class InterestingCompanyController {
         return "mypage/specific/personalFavoriteList";
     }
     @DeleteMapping("specific/delete")
-    public void removeInterestingCompany(){
-
+    public void removeInterestingCompany(@RequestParam("interestingCompanyId") Long interestingCompanyId){
+        interestingCompanyService.interestingCompanyRemove(interestingCompanyId);
     }
 }
