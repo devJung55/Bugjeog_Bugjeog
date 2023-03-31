2fca0034982311f97a94a446d23e9682a9161946
devJung55 <devJung55@gmail.com>
2023-03-31 21:52:58 +0900

    정지영 - Member, Main 작업

--- src/main/java/com/bugjeogbugjeog/app/bugjeogbugjeog/controller/MainController.java
+++ src/main/java/com/bugjeogbugjeog/app/bugjeogbugjeog/controller/MainController.java
@@ -1,15 +1,37 @@
 package com.bugjeogbugjeog.app.bugjeogbugjeog.controller;
 
+import com.bugjeogbugjeog.app.bugjeogbugjeog.domain.vo.MemberVO;
+import com.bugjeogbugjeog.app.bugjeogbugjeog.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpSession;
+
 @Controller
 @RequestMapping("/main/*")
+@RequiredArgsConstructor
+@Slf4j
 public class MainController {
+    private final MemberService memberService;
+
     /*화면 이동*/
     @GetMapping("")    //url 부분
-    public String mainPage(){ return "/main/main";}
+    public String mainPage(HttpSession httpSession){
+        if(httpSession.getAttribute("memberId") != null) {  // 자영업자 로그인 시 분기처리
+            log.info(httpSession.getAttribute("memberId").toString());
+
+        } else if(httpSession.getAttribute("businessId") != null) { // 비지니스 로그인 시 분기처리
+            log.info(httpSession.getAttribute("businessId").toString());
+
+        } else if(httpSession.getAttribute("memberVO") != null){    // 카카오 및 네이버 로그인 시 분기처리
+            MemberVO memberVO = (MemberVO)httpSession.getAttribute("memberVO");
+            httpSession.setAttribute("memberId", memberService.loginNaverOauth(memberVO.getMemberEmail()));
+        }
+        return "/main/main";
+    }
     /* http://localhost:10000/main/ */
 
 }
