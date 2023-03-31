1112975098a4b7c57158e0ff3d420b43015213ed
dev-Sungyu <tjfdhs364@gmail.com>
2023-03-31 19:57:40 +0900

    중간 커밋

---
+++ src/main/java/com/bugjeogbugjeog/app/bugjeogbugjeog/domain/vo/InterestingCriteria.java
@@ -0,0 +1,33 @@
+package com.bugjeogbugjeog.app.bugjeogbugjeog.domain.vo;
+
+import lombok.Data;
+import org.springframework.stereotype.Component;
+import org.springframework.web.util.UriComponentsBuilder;
+
+@Component
+@Data
+public class InterestingCriteria {
+    private Integer pageNum;
+    private Integer amount;
+
+    public InterestingCriteria() {
+        this(1, 9);
+    }
+
+    public InterestingCriteria(Integer pageNum, Integer amount) {
+        this.pageNum = pageNum;
+        this.amount = amount;
+    }
+    public String getListLink() {
+        UriComponentsBuilder builder = UriComponentsBuilder.fromPath("")
+                .queryParam("pageNum", this.pageNum)
+                .queryParam("amount", this.amount);
+
+        return builder.toUriString();
+    }
+
+    public int getStartRow(){
+        return (this.pageNum -1) * this.amount;
+    }
+
+}
