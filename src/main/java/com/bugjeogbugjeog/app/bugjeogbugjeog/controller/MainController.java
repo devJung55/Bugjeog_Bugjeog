32198e4cf53ab5071269f2209eec427a0c1b4333
devJung55 <devJung55@gmail.com>
2023-04-02 17:43:06 +0900

    정지영 - 작업

--- src/main/java/com/bugjeogbugjeog/app/bugjeogbugjeog/controller/MainController.java
+++ src/main/java/com/bugjeogbugjeog/app/bugjeogbugjeog/controller/MainController.java
@@ -21,14 +21,13 @@ public class MainController {
     @GetMapping("")    //url 부분
     public String mainPage(HttpSession httpSession){
         if(httpSession.getAttribute("memberId") != null) {  // 자영업자 로그인 시 분기처리
-            log.info(httpSession.getAttribute("memberId").toString());
+            httpSession.getAttribute("memberId");
 
         } else if(httpSession.getAttribute("businessId") != null) { // 비지니스 로그인 시 분기처리
-            log.info(httpSession.getAttribute("businessId").toString());
+            httpSession.getAttribute("businessId").toString();
 
         } else if(httpSession.getAttribute("memberVO") != null){    // 카카오 및 네이버 로그인 시 분기처리
-            MemberVO memberVO = (MemberVO)httpSession.getAttribute("memberVO");
-            httpSession.setAttribute("memberId", memberService.loginNaverOauth(memberVO.getMemberEmail()));
+            httpSession.setAttribute("memberId", memberService.loginNaverOauth(((MemberVO)httpSession.getAttribute("memberVO")).getMemberEmail()));
         }
         return "/main/main";
     }
