f8bcd67019275871335ed70d537157cb8bc0ef7d
LeeSoonCheol <2sooncheol@gmail.com>
2023-04-03 16:48:15 +0900

    관리자 문의 게시판 리스트 제외 완료

--- src/main/java/com/bugjeogbugjeog/app/bugjeogbugjeog/mapper/InquiryBoardMapper.java
+++ src/main/java/com/bugjeogbugjeog/app/bugjeogbugjeog/mapper/InquiryBoardMapper.java
@@ -1,5 +1,7 @@
 package com.bugjeogbugjeog.app.bugjeogbugjeog.mapper;
 
+import com.bugjeogbugjeog.app.bugjeogbugjeog.domain.dto.AdminCriteria;
+import com.bugjeogbugjeog.app.bugjeogbugjeog.domain.dto.InquiryDTO;
 import com.bugjeogbugjeog.app.bugjeogbugjeog.domain.vo.BoardInquiryVO;
 import com.bugjeogbugjeog.app.bugjeogbugjeog.domain.vo.Criteria;
 import org.apache.ibatis.annotations.Mapper;
@@ -24,4 +26,19 @@ public interface InquiryBoardMapper {
 
     //    문의 게시글 작성 갯수
     public Integer businessInquireCount(Long businessId);
+
+    /* 관리자 ------------------------------------------------------------------------ */
+
+    // 문의 목록
+    public List<BoardInquiryVO> adminSelectAllInquiry(AdminCriteria adminCriteria);
+
+    // 문의 조회
+    public InquiryDTO adminSelectInquiry(Long boardInquiryId);
+
+    // 문의 삭제
+    public void delete(Long boardInquiryId);
+
+    // 문의 카운트
+    public int count();
+
 }
--- src/main/java/com/bugjeogbugjeog/app/bugjeogbugjeog/service/InquiryBoardService.java
+++ src/main/java/com/bugjeogbugjeog/app/bugjeogbugjeog/service/InquiryBoardService.java
@@ -1,7 +1,34 @@
 package com.bugjeogbugjeog.app.bugjeogbugjeog.service;
 
+import com.bugjeogbugjeog.app.bugjeogbugjeog.domain.dao.InquiryBoardDAO;
+import com.bugjeogbugjeog.app.bugjeogbugjeog.domain.dto.AdminCriteria;
+import com.bugjeogbugjeog.app.bugjeogbugjeog.domain.dto.InquiryDTO;
+import com.bugjeogbugjeog.app.bugjeogbugjeog.domain.vo.BoardInquiryVO;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
+@RequiredArgsConstructor
 public class InquiryBoardService {
+    private final InquiryBoardDAO inquiryBoardDAO;
+
+    /* 관리자 ------------------------------------------------------------------------ */
+
+    // 문의 목록
+    public List<BoardInquiryVO> adminFindAll(AdminCriteria adminCriteria){
+        return inquiryBoardDAO.getInquiryList(adminCriteria);}
+
+    // 문의 조회
+    public InquiryDTO adminFindByInquiry(Long boardInquiryId){
+        return inquiryBoardDAO.getInquiry(boardInquiryId);}
+
+    // 문의 삭제
+    public void removeInquiry(List<String> boardInquiryIds){
+        boardInquiryIds.stream().map(boardInquiryId -> Long.parseLong(boardInquiryId)).forEach(inquiryBoardDAO::deleteInquiry);
+    }
+
+    // 문의 카운트
+    public int count(){return inquiryBoardDAO.count();}
 }
