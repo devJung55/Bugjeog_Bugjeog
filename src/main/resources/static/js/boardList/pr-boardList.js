a2784861908d0b6274c9634f8c9eaedc35fef803
LeeJunSang <miraclelee0613@gmail.com>
2023-04-03 09:35:57 +0900

    보드 수정중

--- src/main/resources/static/js/boardList/pr-boardList.js
+++ src/main/resources/static/js/boardList/pr-boardList.js
@@ -14,14 +14,14 @@ $(window).scroll(function () {
 });
 
 // /* sort 버튼 이벤트 */
-// const $sortButton = $(".sort-button");
-//
-// $sortButton.each((i, e) => {
-//     $(e).click(function () {
-//         $sortButton.removeClass("sort-button-active");
-//         $(e).addClass("sort-button-active");
-//     });
-// });
+const $sortButton = $(".sort-button");
+
+$sortButton.each((i, e) => {
+    $(e).click(function () {
+        $sortButton.removeClass("sort-button-active");
+        $(e).addClass("sort-button-active");
+    });
+});
 
 
 // /* 카테고리 버튼 이벤트 */
