de6fe33abe6756730f10b7214b465eefba322553
dev-Sungyu <tjfdhs364@gmail.com>
2023-04-03 14:27:18 +0900

    중간 커밋

--- src/main/java/com/bugjeogbugjeog/app/bugjeogbugjeog/controller/InterestingCompanyRestController.java
+++ src/main/java/com/bugjeogbugjeog/app/bugjeogbugjeog/controller/InterestingCompanyRestController.java
@@ -1,8 +1,27 @@
 package com.bugjeogbugjeog.app.bugjeogbugjeog.controller;
 
 
-import org.springframework.web.bind.annotation.RestController;
+import com.bugjeogbugjeog.app.bugjeogbugjeog.domain.dto.InterestingCompanyDTO;
+import com.bugjeogbugjeog.app.bugjeogbugjeog.service.InterestingCompanyService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequiredArgsConstructor
+@RequestMapping("/mypage/*")
 public class InterestingCompanyRestController {
+    private final InterestingCompanyService interestingCompanyService;
+
+    @DeleteMapping("specific/personalFavoriteList")
+    public ResponseEntity<Void> deleteInterestingCompany(@RequestParam("interestingCompanyId") Long interestingCompanyId, @RequestParam("memberId") Long memberId) {
+        interestingCompanyService.deleteInterestingCompany(interestingCompanyId, memberId);
+        return ResponseEntity.noContent().build();
+    }
+    @PostMapping("specific/personalFavoriteList")
+    public ResponseEntity<Void> insertInterestingCompany(@RequestBody InterestingCompanyDTO interestingCompanyDTO) {
+        interestingCompanyService.insertInterestingCompany(interestingCompanyDTO.getInterestingCompanyId(), interestingCompanyDTO.getMemberId());
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
 }
