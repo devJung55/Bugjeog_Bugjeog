8bfb63c25f266357805955886f5059d646563544
LeeSoonCheol <2sooncheol@gmail.com>
2023-04-05 21:03:56 +0900

    자유 게시판 백엔드 추가 작업 진행 중

--- src/main/java/com/bugjeogbugjeog/app/bugjeogbugjeog/controller/InquiryBoardController.java
+++ src/main/java/com/bugjeogbugjeog/app/bugjeogbugjeog/controller/InquiryBoardController.java
@@ -1,13 +1,11 @@
 package com.bugjeogbugjeog.app.bugjeogbugjeog.controller;
 
 import com.bugjeogbugjeog.app.bugjeogbugjeog.domain.dto.BoardInquiryDTO;
-import com.bugjeogbugjeog.app.bugjeogbugjeog.domain.dto.PageDTO;
-import com.bugjeogbugjeog.app.bugjeogbugjeog.domain.vo.BoardBusinessVO;
+import com.bugjeogbugjeog.app.bugjeogbugjeog.domain.dto.InquiryPageDTO;
 import com.bugjeogbugjeog.app.bugjeogbugjeog.domain.vo.BoardInquiryVO;
 import com.bugjeogbugjeog.app.bugjeogbugjeog.domain.vo.Criteria;
 import com.bugjeogbugjeog.app.bugjeogbugjeog.service.InquiryAnswerService;
 import com.bugjeogbugjeog.app.bugjeogbugjeog.service.InquiryBoardService;
-import com.bugjeogbugjeog.app.bugjeogbugjeog.service.InquiryCriteria;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -19,6 +17,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.view.RedirectView;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 @Controller
 //@RequestMapping("/board/inquiry")
@@ -35,9 +34,12 @@ public class InquiryBoardController {
 
     //    리스트
     @GetMapping("/board/inquiry/list")
-    public void showList(Model model, InquiryCriteria inquiryCriteria) {
-        model.addAttribute("pageDTO", new PageDTO(inquiryCriteria, Integer.parseInt(inquiryBoardService.count().toString())));
-        model.addAttribute("boards", inquiryBoardService.showList(inquiryCriteria));
+    public void showList(Model model, Criteria criteria) {
+
+        criteria = new Criteria(1,10);
+
+        model.addAttribute("inquiryVO",inquiryBoardService.getAllList(criteria));
+        model.addAttribute("pageDTO", new InquiryPageDTO(criteria, inquiryBoardService.count().intValue()));
     }
 
     @GetMapping("/board/inquiry/list/ajax")
@@ -61,22 +63,18 @@ public class InquiryBoardController {
 
     @PostMapping("/board/inquiry/write")
     public RedirectView write(BoardInquiryVO boardInquiryVO, HttpServletRequest req) {
-        System.out.println(boardInquiryVO.getBoardInquiryStatus());
-        if (!req.getParameter("businessId").isEmpty() || !req.getParameter("memberId").isEmpty()) {
-            boardInquiryVO.setBusinessId(Long.parseLong(req.getParameter("businessId")));
-            boardInquiryVO.setMemberId(Long.parseLong(req.getParameter("memberId")));
-            inquiryBoardService.registerBoard(boardInquiryVO);
+        HttpSession session = req.getSession();
+        Long memberId = (Long) session.getAttribute("memberId");
+        Long businessId = (Long) session.getAttribute("businessId");
+
+        if(memberId != null){
+            boardInquiryVO.setMemberId(memberId);
+        }else {
+            boardInquiryVO.setBusinessId(businessId);
         }
-//        if (req.getParameter("businessId") != null) {
-////            boardInquiryVO.setBusinessId(Long.parseLong(req.getParameter("businessId")));
-//            boardInquiryVO.setBusinessId(Long.parseLong(req.getParameter("businessId")));
-//            boardInquiryVO.setMemberId(null);
-//            inquiryBoardService.registerBoard(boardInquiryVO, "business");
-//        } else {
-//            boardInquiryVO.setBusinessId(null);
-//            boardInquiryVO.setMemberId(Long.parseLong(req.getParameter("memberId")));
-//            inquiryBoardService.registerBoard(boardInquiryVO, "member");
-//        }
+
+        inquiryBoardService.registerBoard(boardInquiryVO);
+
         return new RedirectView("/board/inquiry/list");
     }
 }
